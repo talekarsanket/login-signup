bedaa4c0a9226125febb982334398a5e3f73aafa
talekarsanket <talekarsanket9@gmail.com>
2023-11-06 19:22:11 +0530

    updated code

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Dbconnection = async () => {
 
     try {
-        await mongoose.connect('mongodb+srv://sanket:sanket@cluster0.ntdskhz.mongodb.net/user_register');
+        await mongoose.connect('mongodb+srv://sanket:sanket@cluster0.ntdskhz.mongodb.net/');
         console.log("Database is connected");
         return;
     } catch (error) {
